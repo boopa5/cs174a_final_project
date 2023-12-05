41b2b1db10cf79dc7dc829110e168fe403eb7e58
Johnson Chan <c_chan@ucsb.edu>
2023-12-04 16:56:20 -0800

    fixed StockAccount and added Stoick

---
+++ src/my_package/Stock.java
@@ -0,0 +1,52 @@
+package my_package;
+
+public class Stock {
+        public Stock(String symbol, float closing_price, float current_price, int id) {
+            this.symbol = symbol;
+            this.closing_price = closing_price;
+            this.current_price = current_price;
+            this.id = id;
+        }
+
+        public float getClosingPrice() {
+            return closing_price;
+        }
+
+        public float getCurrentPrice() {
+            return current_price;
+        }
+
+        public int getid() {
+            return id;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setClosingPrice(float closing_price) {
+            this.closing_price = closing_price;
+        }
+
+        public void setCurrentPrice(float current_price) {
+            this.current_price = current_price;
+        }
+
+        public void setSymbol(String symbol) {
+            this.symbol = symbol;
+        }
+        @Override
+        public String toString() {
+            return "[id=" + id + ", closing_price=" + closing_price + ", current_price=" + current_price + ", symbol=" + symbol + "]";
+        }
+
+        //String symbol, float closing_price, float current_price, int id
+        private int id;
+        private float closing_price;
+        private float current_price;
+        private String symbol;
+    }
--- src/my_package/StockAccount.java
+++ src/my_package/StockAccount.java
@@ -1,12 +1,12 @@
 package my_package;
 
 public class StockAccount {
-        public StockAccount(int sid, String symbols, Stock stock, CustomerProfile customer_profile, String username) {
+        public StockAccount(int sid, String symbols, String username) {
             this.sid = sid;
             this.username = username;
             this.symbols = symbols;
-            this.stock = stock;
-            this.customer_profile = customer_profile;
+            //this.stock = stock;
+            //this.customer_profile = customer_profile;
         }
 
         public int getSid() {
@@ -21,12 +21,13 @@ public class StockAccount {
             return symbols;
         }
 
-        public Stock getStock() {
-            return stock;
+        //GetStock TODO
+        public void getStock() {
+            //return stock;
         }
 
-        public CustomerProfile getCustomerProfile() {
-            return customer_profile;
+        //GetCustomerProfile TODO
+        public void getCustomerProfile() {
         }
 
         public void setSid(int sid) {
@@ -40,25 +41,26 @@ public class StockAccount {
         public void setSymbols(String symbols) {
             this.symbols = symbols;
         }
-
+        /*
         public void setStock(Stock stock) {
             this.stock = stock;
         }
-
+        
         public void setCustomerProfile(CustomerProfile customer_profile) {
             this.customer_profile = customer_profile;
         }
-
+        */
 
         @Override
         public String toString() {
-            return "[sid=" + id + ", username=" + username + ", symbols=" + symbols + ", Stock=" + stock.toString() ", Customer Profile=" + customer_profile.toString() + + "]";
+            //+ ", Stock=" + stock.toString() ", Customer Profile=" + customer_profile.toString()
+            return "[sid=" + id + ", username=" + username + ", symbols=" + symbols  +"]";
         }
 
 
         private int sid;
         private String username;
         private String symbols;
-        private Stock stock;
-        private CustomerProfile customer_profile;
+        //private Stock stock;
+        //private CustomerProfile customer_profile;
     }
