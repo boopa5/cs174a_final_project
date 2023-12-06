4b6e627dd23b3bff2d99b8cee0d5714b651b64ec
Johnson Chan <c_chan@ucsb.edu>
2023-12-05 18:12:52 -0800

    completed skeleton code for manager and trader interfaces

---
+++ src/my_package/ManagerInterface.java
@@ -0,0 +1,28 @@
+package my_package;
+
+public class ManagerInterface {
+    //TODO
+    public void addInterest(String customerUserName){
+
+    }
+    //TODO
+    public void getMonthlyStatement(){
+
+    }
+    //TODO
+    public void getActiveCustomer(){
+
+    }
+    //TODO
+    public void DTER(){
+
+    }
+    //TODO
+    public void getCustomerReport(String customerUsername){
+
+    }
+    //TODO
+    public void delTransactions(){
+        
+    }
+}
---
+++ src/my_package/TraderInterface.java
@@ -0,0 +1,58 @@
+package my_package;
+
+public class TraderInterface {
+
+    public TraderInterface(String customerUserName){
+        this.customerUserName = customerUserName;
+    }
+
+    //TODO
+    public void register(String username, String name, String pw, String state, String email, String
+phone_number, String tax_id, String date){
+
+    }
+    //TODO
+    public void deposit(double amount){
+
+    }
+    //TODO
+    public void withdraw(double amount){
+
+    }
+    //TODO
+    public void buy(String stock_symbol, double amount){
+
+    }
+    //TODO
+    public void sell(String stock_symbol, double price, double amount){
+
+    }
+    //TODO
+    public void cancel(int tid){
+
+    }
+    //TODO
+    public void showMarketBalance(){
+
+    }
+    //TODO
+    public void showStockHistory(){
+
+    }
+    //TODO
+    public void stockInfo(String symbol){
+
+    }
+    //TODO
+    public void movieInfo(String name, int year){
+
+    }
+    //TODO
+    public void topMovies(String date1, String date2){
+
+    }
+    //TODO
+    public void reviews(String, name, int year){
+        
+    }
+}
